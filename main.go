d4df4d0d9101718f7c51d3f69c08e559c65e3c73
CRowland4 <cjrowland4@gmail.com>
2024-03-10 10:09:26 -0500

    Commit before working on next problem

--- main.go
+++ main.go
@@ -6,20 +6,19 @@ import (
 )
 
 type apiConfig struct {
-	fileServerHits int
+	fileserverHits int
 }
 
 func main() {
 	apiCfg := apiConfig{
-		fileServerHits: 0,
+		fileserverHits: 0,
 	}
 	mux := http.NewServeMux() // A "mux" or "multiplexer" is synonymous with "router"
 
 	// This piece - http.StripPrefix("/app", http.FileServer(http.Dir("."))) - returns a http.Handler that serves up the index.html page.
 	// The middlewReMetricsInc piece returns a http.Handler that just adds to the apiCfg fileServerHits count, and then executes the ServeHTTP(w, r) of the http.Handler
 	//     that it's given - in this case, the one returned by the StripPrefix
-	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))) // Interpretation: For the "/" pattern or path, use http.FileServer(http.Dir(".")) to return an http.Handler
-
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	// mux.HandleFunc takes a path, and then a handler function - the handler function just needs to have the signature <name>(http.ResponseWriter, *http.Request)
 	// The mux.HandleFunc call handles the execution of the passed handler function when the given path is called
 	mux.HandleFunc("/healthz", handlerReadiness)
@@ -33,14 +32,6 @@ func main() {
 	server.ListenAndServe()
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			cfg.fileServerHits++
-			next.ServeHTTP(w, r)
-		})
-}
-
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)                    // This is for the status code of the HTTP response
@@ -50,14 +41,23 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handlerFileServerHitsCounter(w http.ResponseWriter, r *http.Request) {
-	hits := fmt.Sprintf("Hits: %d", cfg.fileServerHits)
+	hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(hits))
 	return
 }
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		w.Header().Set("Cache-Control", "no-cache")
+		next.ServeHTTP(w, r)
+	})
+}
+
+
 func (cfg *apiConfig) handlerResetFileServerHits(w http.ResponseWriter, r *http.Request) {
-	cfg.fileServerHits = 0
+	cfg.fileserverHits = 0
 	return
 }
 
